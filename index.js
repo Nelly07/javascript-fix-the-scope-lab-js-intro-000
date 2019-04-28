dbff9caea864be0ce0bac80c6529a1d20aff3899
Nelly Khmeleva <nkhmeleva@yahoo.com>
2019-04-28 18:44:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var animal = 'dog'
 
-
+function FixtheScope(){
 function myAnimal() {
   return animal
 }
@@ -34,5 +34,6 @@ var funkyFunction=function(){
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction()
+var theFunk = funkyFunction()();
 theFunk();
+}
